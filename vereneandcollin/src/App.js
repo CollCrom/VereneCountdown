2108a96bc967bad86439881d074d40330c506fbb
Collin Cromwell <collcrom@gmail.com>
2018-11-21 13:23:28 -0500

    updated TZ

--- vereneandcollin/src/App.js
+++ vereneandcollin/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown'
+import moment from 'moment-timezone';
 
-
-const dayToSeeVerene = new Date('Nov 16, 2018 18:46:00').getTime()
+const tz = moment.tz('Dec 26, 2018 12:50:00', 'America/Denver')
+const dayToSeeVerene = new Date(tz).getTime()
 
 class App extends Component {
   render() {
